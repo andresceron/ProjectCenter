4569781a33b5b44fe8b2e16586fb15ab6c71ecec
andresceron <andres.ceron@me.com>
2015-11-04 01:29:39 +0100

    Deleting some files

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-		<footer class="footer">
-			<p class="text-center">Copyright &copy; 2015 | Andres Ceron</p>
-		</footer>
-	</body>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</html> 
