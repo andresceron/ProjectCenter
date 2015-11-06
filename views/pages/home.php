d92933d7a7e0a7736487e8656f14f7b6a9f942cd
andresceron <andres.ceron@me.com>
2015-11-06 17:31:18 +0100

    Fixing the HTML and adding Bootstraps accordion to the sections.

--- views/pages/home.php
+++ views/pages/home.php
@@ -45,90 +45,120 @@
 			</div>
 			<div class="col-md-8">
 				<div class="panel panel-default">
-					<div class="panel-heading">My Active Projects</div>
-					<div class="panel-body">
-						<?php //TODO: Generate the active projects ?>
-						<div class="list-group">
-							<div class="project-lists">
-								<?php $projRows = $user->activeProjects($user_id); ?>
-								<?php if ($projRows === false): ?>
-									<span>No active projects</span>
-								<?php else: ?>
-									<?php foreach ($projRows as $row): ?>
+					<div class="panel-heading" role="tab">
+						<h4 class="panel-title">
+					        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#activeProjects">
+			         			Active projects
+					        </a>
+				    	</h4>
+					</div>
+					<div id="activeProjects" class="panel-collapse collapse in" role="tabpanel">
+						<div class="panel-body">
+							<div class="list-group">
+								<div class="project-lists">
+									<?php $projRows = $user->upcomingProjects($user_id); ?>
+									<?php if ($projRows === false): ?>
+										<span>No upcoming projects yet</span>
+									<?php else: ?>
+										<?php foreach ($projRows as $row): ?>
+											<form method="post" action="home.php">
+											<button name="projectDetail"><?= $row['proj_name']; ?>
+												<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
+											</button>
+											<span><?= $row['proj_date_start'] ?></span>
+											<hr>
+											</form>
+										<?php endforeach; ?>
+									<?php endif; ?>
+								</div>
+							</div>
+					  	</div>
+				  	</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab">
+						<h4 class="panel-title">
+					        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#upcomingProjects">
+			         			Upcoming projects
+					        </a>
+				    	</h4>
+					</div>
+					<div id="upcomingProjects" class="panel-collapse collapse" role="tabpanel">
+						<div class="panel-body">
+							<div class="list-group">
+								<div class="project-lists">
+									<?php $projRows = $user->upcomingProjects($user_id); ?>
+									<?php if ($projRows === false): ?>
+										<span>No upcoming projects yet</span>
+									<?php else: ?>
+										<?php foreach ($projRows as $row): ?>
+											<form method="post" action="home.php">
+											<button name="projectDetail"><?= $row['proj_name']; ?>
+												<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
+											</button>
+											<span><?= $row['proj_date_start'] ?></span>
+											<hr>
+											</form>
+										<?php endforeach; ?>
+									<?php endif; ?>
+								</div>
+							</div>
+					  	</div>
+				  	</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab">
+						<h4 class="panel-title">
+					        <a class="collapsed" role="button" data-toggle="collapse" href="#previousProjects">
+			         			Previous projects
+					        </a>
+				    	</h4>
+					</div>
+					<div id="previousProjects" class="panel-collapse collapse" role="tabpanel">
+						<div class="panel-body">
+							<div class="list-group">
+								<div class="project-lists">
+									<?php $projRows = $user->previousProjects($user_id); ?>	
+									<?php foreach ($projRows as $row): ?>	
+										<form method="post" action="home.php">
+											<button name="projectDetail"><?= $row['proj_name']; ?>
+												<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
+											</button>
+											<span><?= $row['proj_date_start'] . " - " . $row['proj_date_end'] ?></span>
+											<hr>
+										</form>	
+									<?php endforeach;?>
+								</div>
+							</div>
+					  	</div>
+				  	</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab">
+						<h4 class="panel-title">
+					        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+								All projects
+					        </a>
+				    	</h4>
+					</div>
+					<div id="collapseThree" class="panel-collapse collapse" role="tabpanel">
+						<div class="panel-body">
+							<div class="list-group">
+								<div class="project-lists">
+									<?php $variable = $user->allProjects(); ?>
+									<?php foreach ($variable as $row): ?>	
 										<form method="post" action="home.php">
 										<button name="projectDetail"><?= $row['proj_name']; ?>
 											<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
 										</button>
-										<span><?= $row['proj_date_start'] ?></span>
 										<hr>
 										</form>
-									<?php endforeach; ?>
-								<?php endif; ?>
+									<?php endforeach;?>
+								</div>
 							</div>
-						</div>
-				  	</div>
-				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">Upcoming Projects</div>
-					<div class="panel-body">
-						<?php //TODO: Generate the previous projects ?>
-						<div class="list-group">
-							<div class="project-lists">
-								<?php $projRows = $user->upcomingProjects($user_id); ?>
-								<?php if ($projRows === false): ?>
-									<span>No upcoming projects yet</span>
-								<?php else: ?>
-									<?php foreach ($projRows as $row): ?>
-										<form method="post" action="home.php">
-										<button name="projectDetail"><?= $row['proj_name']; ?>
-											<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
-										</button>
-										<span><?= $row['proj_date_start'] ?></span>
-										<hr>
-										</form>
-									<?php endforeach; ?>
-								<?php endif; ?>
-							</div>
-						</div>
-				  	</div>
-				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">Previous Projects</div>
-					<div class="panel-body">
-						<?php //TODO: Generate the previous projects ?> 
-						<div class="list-group">
-							<div class="project-lists">
-								<?php $projRows = $user->previousProjects($user_id); ?>	
-								<?php foreach ($projRows as $row): ?>	
-									<form method="post" action="home.php">
-										<button name="projectDetail"><?= $row['proj_name']; ?>
-											<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
-										</button>
-										<span><?= $row['proj_date_start'] . " - " . $row['proj_date_end'] ?></span>
-										<hr>
-									</form>	
-								<?php endforeach;?>
-							</div>
-						</div>
-				  	</div>
-				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">All Projects</div>
-					<div class="panel-body">
-						<?php //TODO: Generate the previous projects ?>
-						<div class="list-group">
-							<div class="project-lists">
-								<?php $variable = $user->allProjects(); ?>
-								<?php foreach ($variable as $row): ?>	
-									<form method="post" action="home.php">
-									<button name="projectDetail"><?= $row['proj_name']; ?>
-										<input type="hidden" name="proj_id" value="<?= $row['proj_id'] ?>">
-									</button>
-									<hr>
-									</form>
-								<?php endforeach;?>
-							</div>
-						</div>
+					  	</div>
 				  	</div>
 				</div>
 			</div>
