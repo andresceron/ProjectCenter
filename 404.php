726467bd59623c8aef9506520271e3a1f49b0546
andresceron <andres.ceron@me.com>
2015-11-07 14:50:04 +0100

    Added .htaccess and 404 redirect file for bad requests

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php // basic 404 error pageheader('HTTP/1.1 404 Not Found');header('Status: 404 Not Found'); 
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Page not found</title>
+	</head>
+	<body>
+		<h1>Page not found</h1>
+		<p>Sorry, we cannot find that page.</p>
+		<p>
+			<a href="/">Please return to the home page&hellip;</a>
+		</p>
+	</body>
+</html>
